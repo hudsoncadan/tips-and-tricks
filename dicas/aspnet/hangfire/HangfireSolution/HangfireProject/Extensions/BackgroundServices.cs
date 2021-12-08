4eaf586e7ffce65ca89cef41fa4ba80f8ebf7ff0
Hudson Cadan Scheffer <hudsoncadan@gmail.com>
2021-12-08 18:13:43 -0300

    Dotnet/hangfire (#7)
    * Incluir dica Hangfire

---
+++ dicas/aspnet/hangfire/HangfireSolution/HangfireProject/Extensions/BackgroundServices.cs
@@ -0,0 +1,46 @@
+﻿using Hangfire;
+using HangfireProject.Services.Background;
+using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HangfireProject.Extensions
+{
+    public static class BackgroundServices
+    {
+        /// <summary>
+        /// Adiciona um serviço recorrente para ser executado a cada 1 minuto
+        /// https://crontab.guru
+        /// </summary>
+        public static IApplicationBuilder AddRecurringBackground(this IApplicationBuilder app)
+        {
+            RecurringJob.AddOrUpdate("Logging a cada 1 minuto", () => ConsoleLogs.EnqueueToWriteInConsoleRecurring(),
+                "*/1 * * * *");
+
+            return app;
+        }
+
+        /// <summary>
+        /// Adiciona um serviço para ser executado uma única vez após X segundos
+        /// </summary>
+        public static IApplicationBuilder AddOneTimeRunBackground(this IApplicationBuilder app, int seconds)
+        {
+            BackgroundJob.Schedule(() => ConsoleLogs.WriteInConsoleOne(),
+                TimeSpan.FromSeconds(seconds));
+
+            return app;
+        }
+
+        /// <summary>
+        /// Adiciona um serviço na fila para ser executado assim que possível
+        /// </summary>
+        public static IApplicationBuilder AddEnqueuedBackground(this IApplicationBuilder app)
+        {
+            BackgroundJob.Enqueue(() => ConsoleLogs.WriteInConsoleTwo());
+
+            return app;
+        }
+    }
+}
---
+++ dicas/aspnet/hangfire/HangfireSolution/HangfireProject/Filters/HangfireAuthorizationFilter.cs
@@ -0,0 +1,21 @@
+﻿using Hangfire.Annotations;
+using Hangfire.Dashboard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HangfireProject.Filters
+{
+    public class HangfireAuthorizationFilter : IDashboardAuthorizationFilter
+    {
+        public bool Authorize([NotNull] DashboardContext context)
+        {
+            //var httpContext = context.GetHttpContext();
+            //return httpContext.User.Identity.IsAuthenticated;
+
+            // Allow all authenticated users to see the Dashboard (potentially dangerous)
+            return true;
+        }
+    }
+}
